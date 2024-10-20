9f892ff09736b1c0e4350854afd67ab39f520c10
Benjamin <benjamin.cerninak@gmail.com>
2024-10-20 18:27:36 +0200

    Add Delete  API endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 config();
-import express, { Request, Response } from "express";
+import express, { Request, response, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
@@ -30,6 +30,12 @@ app.post("/decks", async (req: Request, res: Response) => {
   });
   const createdDeck = await newDeck.save();
   res.json(createdDeck);
+});
+
+app.delete("/decks/:deckId", async (req: Request, res: Response) => {
+  const deckId = req.params.deckId;
+  const deck = await Deck.findByIdAndDelete(deckId);
+  res.json(deck);
 });
 
 const db = mongoose.connect(process.env.MONGO_URL!).then(() => {
