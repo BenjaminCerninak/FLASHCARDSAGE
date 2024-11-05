25670540c2618f06ba79f84e443dd56be4a6c9d1
Benjamin <benjamin.cerninak@gmail.com>
2024-11-05 22:38:35 +0100

    render decks on the landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,24 @@ import { config } from "dotenv";
 config();
 
 import express, { Request, Response } from "express";
-
 import mongoose from "mongoose";
-
+import cors from "cors";
 import Deck from "./models/Deck";
+
 const PORT = 5000;
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
+
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find(); // Získá všechny decky
+  res.json(decks); // Vrátí decky jako JSON
+});
 
 app.post("/decks", async (req: Request, res: Response) => {
   const newDeck = new Deck({
@@ -23,4 +33,3 @@ mongoose.connect(process.env.MONGO_URI!).then(() => {
   console.log(`Server is running on http://localhost:${PORT}`);
   app.listen(PORT);
 });
-console.log("MongoDB URL:", process.env.MONGO_URI);
