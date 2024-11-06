a4ba8532e7b174a06c574f2bacd1eafa055dd764
Benjamin Cerninak <benjamin.cerninak@gmail.com>
2024-11-06 22:45:05 +0100

    add delete endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,6 +29,15 @@ app.post("/decks", async (req: Request, res: Response) => {
   res.json(createdDeck);
 });
 
+app.delete("/decks/:deckId", async (req: Request, res: Response) => {
+ const deckId = req.params.deckId
+
+ const deck = await Deck.findByIdAndDelete(deckId)
+
+ res.json(deck)
+});
+
+
 mongoose.connect(process.env.MONGO_URI!).then(() => {
   console.log(`Server is running on http://localhost:${PORT}`);
   app.listen(PORT);
