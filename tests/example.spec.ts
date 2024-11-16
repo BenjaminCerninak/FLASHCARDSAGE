e86644947f779b51f4c84646805763f4b3973d9f
Benjamin Cerninak <benjamin.cerninak@gmail.com>
2024-11-16 22:35:17 +0100

    update URL for tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-const BASE_URL = "http://localhost:5173";
+const BASE_URL = "https://flashcardsben.cfd/";
 
 test.describe("Home Page", () => {
   test.beforeEach(async ({ page }) => {
